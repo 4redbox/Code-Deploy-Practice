ab25794d6272fc0c35e2d70f25be54c39317c5e4
Saravanesh <raja4redbox@gmail.com>
2024-12-17 20:21:32 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-ls
-pwd
-cd /var/www/myapp
-# Start the Flask application server
-gunicorn -b 0.0.0.0:5000 app:app > /var/www/myapp/logs/gunicorn.log 2>&1 &
+
+# Start the systemd service
+systemctl start myapp.service
+
+# Check service status
+systemctl status myapp.service
